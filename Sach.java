2dd9016e005006e89e0a34805ba41e0317b4672c
Nguyen Hai <lephannguyenhai@gmail.com>
2024-11-08 22:48:32 +0700

    Update Sach.java

--- Sach.java
+++ Sach.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public abstract class Sach {
     private String MaSach;
     private String TenSach;
@@ -45,28 +46,26 @@ public abstract class Sach {
     public void setSoLuong(int SoLuong) {
         this.SoLuong = SoLuong;
     }
-    public void Nhap(){
+    public void Nhap(ArrayList<Sach> dss) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap ma sach: ");
         MaSach = scanner.nextLine();
         boolean check = true;
-        while (check){
-            int count = 0;
-            for(int i = 0; i < DSS.n; i++){
-                if (DSS.dss[i].getMaSach().equals(MaSach)){
+
+        // Kiểm tra mã sách có trùng lặp trong danh sách hay không
+        while (check) {
+            check = false;  // Giả sử mã sách không trùng lặp
+            for (Sach sach : dss) {
+                if (sach.getMaSach().equals(MaSach)) {
                     System.out.println("Ma sach nay da ton tai. Vui long nhap lai!");
                     System.out.print("Nhap ma sach: ");
                     MaSach = scanner.nextLine();
+                    check = true;  // Nếu trùng lặp, yêu cầu nhập lại
                     break;
                 }
-                else {
-                    count++;
-                }
-            }
-            if (count == DSS.n){
-                check = false;
             }
         }
+
         System.out.print("Nhap ten sach: ");
         TenSach = scanner.nextLine();
         System.out.print("Nhap tac gia: ");
@@ -76,17 +75,21 @@ public abstract class Sach {
         System.out.print("Nhap so luong: ");
         SoLuong = scanner.nextInt();
     }
-    public void Xuat(){
+
+    public void Xuat() {
         System.out.println("------------------------");
-        System.out.println("Ma sach "+MaSach);
-        System.out.println("Ten sach "+TenSach);
-        System.out.println("Tac gia "+TacGia);
-        System.out.println("So luong hien co "+SoLuong);
+        System.out.println("Ma sach: " + MaSach);
+        System.out.println("Ten sach: " + TenSach);
+        System.out.println("Tac gia: " + TacGia);
+        System.out.println("Gia: " + Gia);
+        System.out.println("So luong hien co: " + SoLuong);
     }
+
     @Override
     public String toString(){
         return MaSach+","+TenSach+","+TacGia+","+Gia+","+SoLuong;
     }
     public abstract void InThongTin();
+
 }
 
