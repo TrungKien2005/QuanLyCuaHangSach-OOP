ff30341fcf52be85308055ec10b658e83df470cb
Buumazin <148322911+Buumazin@users.noreply.github.com>
2024-11-18 16:28:39 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,51 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Tạo đối tượng DSNV (Danh Sách Nhân Viên)
-        DSNV dsNhanVien = new DSNV();
-        
-        // Gọi menu từ lớp DSNV để bắt đầu các chức năng
-        dsNhanVien.Menu();
+        Scanner sc = new Scanner(System.in);
+        DSS dss = new DSS();
+        DSNV dsnv = new DSNV();
+        DSKH dskh = new DSKH();
+
+        dss.Docfile();
+        dsnv.Docfile();
+        dskh.Docfile();
+        // Truyền danh sách vào DSHD
+        DSHD dshd = new DSHD(dss.getDSS(), dskh.getDSKH(), dsnv.getDSNV());
+        boolean check = true;
+        while(check)
+        {
+            System.out.println("Nhập lựa chọn của bạn: 1.Sách 2.Nhân viên 3.Khách hàng 4.Hóa đơn 5.Thống kê 0.Thoát");
+            int luachon = sc.nextInt();
+            switch(luachon)
+            {
+                case 1:
+                {
+                    dss.Menu();
+                    break;
+                }
+                case 2:
+                {
+                    dsnv.Menu();
+                    break;
+                }
+                case 3:
+                {
+                    dskh.Menu();
+                    break;
+                }
+                case 4:
+                {
+                    dshd.Menu();
+                    break;
+                }
+                default:
+                {
+                    check = false;
+                    break;
+                }
+            }
+        }
     }
 }
